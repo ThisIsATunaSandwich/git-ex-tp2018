ab0518090385add77af867cc1d04f7adbbcda787
gruppe10 <gruppe10@wks04.isf.cs.tu-bs.de>
2018-09-17 12:10:46 +0200

    First Branch: LetsAddSomeCheese

--- HelloTeamproject/src/application/GuiList.java
+++ HelloTeamproject/src/application/GuiList.java
@@ -13,7 +13,7 @@ import javax.swing.event.ListSelectionListener;
 public class GuiList extends JPanel {
 	private static final long serialVersionUID = 1L;
 
-	private String label[] = { "Thomas", "Alex", "Lennart", "Ajeeth"};
+	private String label[] = { "Lennart", "Ajeeth", "Thomas", "Alex" };
 
 	private JList<String> list;
 
